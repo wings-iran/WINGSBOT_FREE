6285ee0e8ef3a1ed5fb3d1fad5a56f940ea05133
WINGS <ir.mil4d@gmail.com>
2025-08-26 11:23:55 +0330

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -10,12 +10,12 @@ logging.basicConfig(
 logger = logging.getLogger("bot")
 
 # --- Basic Settings (env-overridable) ---
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7910215097:AAH-Zalti5nDFPTS8Dokw0Tgcgb3EpibGEc")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "6839887159"))
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@wings_iran")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001553094061"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your bot token")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "your id"))
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "your channle id")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "channle id"))
 DB_NAME = os.getenv("DB_NAME", "bot.db")
-NOBITEX_TOKEN = os.getenv("NOBITEX_TOKEN", "79df9500f80eff5520a73f1414594028ca91daa6")
+NOBITEX_TOKEN = os.getenv("NOBITEX_TOKEN", "nobitex token")
 
 # Job schedule hour for daily tasks
 DAILY_JOB_HOUR = int(os.getenv("DAILY_JOB_HOUR", "9"))
